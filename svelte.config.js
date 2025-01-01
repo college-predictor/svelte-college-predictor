9df5d66f9d9ea42973dd23cb009bdb6315102b18
Sarvesh Singh Chauhan <sarvesh@visionias.in>
2025-01-01 11:07:43 +0530

    removed default path

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: '/engineering' // Replace '/myapp' with your desired base path
-		  }
 	}
 };
 
