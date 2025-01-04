0b4a334a7c6c05c568704ad67d1b2be83a40e843
college-predictor <aicollegepredictor@gmail.com>
2025-01-04 17:50:26 +0530

    changed api endpoint

--- src/routes/college-list/+page.svelte
+++ src/routes/college-list/+page.svelte
@@ -93,7 +93,7 @@
       console.log('Fetching colleges with parameters:', filteredQueryParams);
 
       // Construct the API URL with query parameters
-      const apiUrl = new URL('http://34.69.110.177/api/colleges');
+      const apiUrl = new URL('https://api.collegepredictor.co.in/api/colleges');
       Object.entries(filteredQueryParams).forEach(([key, value]) => {
         apiUrl.searchParams.append(key, value.toString());
       });
