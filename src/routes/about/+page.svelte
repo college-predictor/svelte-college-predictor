907508e49a0ce9eec218d0920f6b4009d0dc090b
college-predictor <aicollegepredictor@gmail.com>
2025-01-05 15:06:37 +0530

    added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<!-- src/components/CollegeList.svelte -->
+<script lang="ts">
+</script>
+
+<style>
+  /* You can add additional styles here if needed */
+</style>
+
+<h1>Welcome to about page!</h1>
