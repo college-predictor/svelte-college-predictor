b82c386cc0428b92f8bad07d08124c496878e2de
college-predictor <aicollegepredictor@gmail.com>
2025-01-14 22:41:38 +0530

    removed year 2016 and 2017 from the website

--- src/routes/college-list/+page.svelte
+++ src/routes/college-list/+page.svelte
@@ -196,8 +196,8 @@
               <option value="2020">2020</option>
               <option value="2019">2019</option>
               <option value="2018">2018</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
+              <!-- <option value="2017">2017</option>
+              <option value="2016">2016</option> -->
             </select>
           </div>
           <!-- NIRF Ranking Filter -->
